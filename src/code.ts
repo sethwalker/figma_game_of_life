3e3828068b0347a090410980b1e4f7d432686014
Seth Walker <seth.h.walker@gmail.com>
2024-07-15 20:18:07 -0400

    performance improvements, configurable speed, ui updates

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,7 @@
 import { random, tick, Grid } from "./grid";
 
 const nodes: SceneNode[] = [];
+const nodePool: SceneNode[] = [];
 let grid: Grid;
 let size: number;
 
@@ -11,11 +12,13 @@ if (figma.editorType === "figjam" || figma.editorType === "figma") {
   // Calls to "parent.postMessage" from within the HTML page will trigger this
   // callback. The callback will be passed the "pluginMessage" property of the
   // posted message.
-  figma.ui.onmessage = (msg: { type: string; size: number }) => {
+  figma.ui.onmessage = (msg: { type: string; size: number; speed: number }) => {
     if (msg.type === "clear") {
       grid && grid.reset();
       clearInterval(timer);
       nodes.forEach((node) => (node.visible = false));
+      nodePool.concat(nodes);
+      nodes.length = 0;
     }
     if (msg.type === "stop") {
       clearInterval(timer);
@@ -35,12 +38,17 @@ if (figma.editorType === "figjam" || figma.editorType === "figma") {
         const numCellsX = Math.round((section ? section.width : 1000) / size);
         const numCellsY = Math.round((section ? section.height : 1000) / size);
         grid = random(numCellsX, numCellsY);
+        section?.children.forEach((child) => {
+          if (child.getPluginData("onfirstrender") === "remove") {
+            child.remove();
+          }
+        });
       }
       render(grid, section);
       timer = setInterval(function () {
         grid = tick(grid);
         render(grid, section);
-      }, 1000);
+      }, msg.speed * 1000);
     }
 
     // Make sure to close the plugin when you're done. Otherwise the plugin will
@@ -56,6 +64,8 @@ function render(grid: Grid, section?: SectionNode) {
   const cellHeight = gridHeight / grid.height;
 
   nodes.forEach((node) => (node.visible = false));
+  nodePool.concat(nodes);
+  nodes.length = 0;
 
   for (const [key, cell] of grid.cells.entries()) {
     if (!cell.populated) {
@@ -64,13 +74,10 @@ function render(grid: Grid, section?: SectionNode) {
     const [x, y] = key.split(",").map(Number);
     let shape;
     if (figma.editorType === "figma") {
-      shape =
-        (nodes.find((node) => !node.visible) as RectangleNode) ||
-        figma.createRectangle();
+      shape = (nodePool.pop() as RectangleNode) || figma.createRectangle();
     } else {
       shape =
-        (nodes.find((node) => !node.visible) as ShapeWithTextNode) ||
-        figma.createShapeWithText();
+        (nodePool.pop() as ShapeWithTextNode) || figma.createShapeWithText();
       shape.shapeType = "SQUARE";
     }
     shape.x = x * cellWidth;
@@ -79,15 +86,14 @@ function render(grid: Grid, section?: SectionNode) {
     // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
     shape.fills = [{ type: "SOLID", color: { r: 1, g: 1, b: 1 } }];
     shape.visible = true;
+    shape.setPluginData("onfirstrender", "remove");
     if (section) {
       section.appendChild(shape);
     } else {
       figma.currentPage.appendChild(shape);
     }
 
-    if (!nodes.includes(shape)) {
-      nodes.push(shape);
-    }
+    nodes.push(shape);
   }
 
   /*
